1e5cecaf549b66c3cfd0ec450b539df37744a3be
AndreyTheHacker <andrejpavlenko666@gmail.com>
2022-02-02 17:19:07 +0900

    Added config, normal hashrate, banner, and more...

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import "os"
 import "math/rand"
 import "net/http"
 import "encoding/json"
+import "reflect"
+import "runtime"
+import "errors"
 
 type Balance struct {
 	Result struct {
@@ -62,6 +65,43 @@ type Stats struct {
 	MinedHashes string
 }
 
+type Currency struct {
+	Date string
+	Usd struct {
+		Ada, Aed, Afm float32
+		Afn, All, Amd, Aoa, Ars, Aud, Awg, Azn float32
+		Bam , Bbd, Bch, Bdt, Bgn, Bhd, Bif float32
+		Bmd, Bnb, Bnd, Bob, Brl, Bsd, Btc, Btn, Bwp float32
+		Byn, Byr, Bzd, Cad, Cdf, Chf, Clf, Clp, Cny float32
+		Cop, Crc, Cuc, Cup, Cve, Czk, Djf, Dkk, Doge float32
+		Dop, Dzd, Egp, Ern, Etb, Etc, Eth, Eur, Fjd float32
+		Fkp, Gbp, Gel, Ggp, Ghs, Gip, Gmd, Gnf, Gtq float32
+		Gyd, Hkd, Hnl, Hrk, Htg, Huf, Idr, Ils, Imp float32
+		Inr, Iqd, Irr, Isk, Jep,bJmd, Jod, Jpy, Kes float32
+		Kgs, Khr, Kmf, Kpw, Krw, Kwd, Kyd, Kzt, Lak float32
+		Lbp, Link, Lkr, Lrd, Isl, Ltc, Itl, Lvl, Lyd float32
+		Mad, Mdl, Mga, Mkd, Mmk, Mnt, Mop, Mro, Mur float32
+		Mvr, Mwk, Mxn, Myr, Mzn, Nad, Ngn, Nio, Nok float32
+		Npr, Nzd, Omr, Pab, Pen, Pgk, Php, Pkr, Pln, Pyg, Qar, Ron, Rsd float32
+		Rub float32
+		Rwf, Sar, Sbd, Scr, Sdg, Sek float32
+		Sgd, Shp, Sll, Sos, Srd, Std, Svc, Syp, Szl, Thb float32
+		Theta, Tjs, Tmt, Tnd, Top, Trx, Try, Ttd, Twd, Tzs float32
+		Uah, Ugx, Usd, Usdt float32
+		Uyu, Uzs, Vef, Vnd, Vuv, Wst, Xaf, Xag float32
+		Xau, Xcd, Xdr, Xlm, Xmr, Xof, Xpf, Xrp float32
+		Yer, Zar, Zmk, Zmw, Zwl float32
+	}
+}
+
+type Configuration struct {
+	Username string `json:"username"`
+	Currency string `json:"currency"`
+	Threads  string `json:"threads"`
+	ThreadsInt int32 `json:"threads"`
+	FeedEvery int32 `json:"feed-every"`
+}
+
 var acc int
 var username string = "ndraey";
 var data Balance
@@ -70,7 +110,18 @@ var pool PoolInfo
 var threads int = 7;
 var priced Stats
 
+var additconv string = "none";
+var currency Currency
+var config Configuration
+
+var defconfigfile string = "config.json";
+var feedevery time.Duration;
+
 func main(){
+	feedevery = time.Duration(45);
+	threads = runtime.NumCPU()
+
+	/*
 	args:=os.Args[1:]
 	for i:=0; i<len(args); i++ {
 		if args[i]=="-t" { //threads
@@ -92,24 +143,67 @@ func main(){
 	if len(args)>=1 {
 		username = args[len(args)-1]
 	}
+
+	*/
+
+	if _, err := os.Stat(defconfigfile); errors.Is(err, os.ErrNotExist) {
+		file, err2 := os.Create("config.json")
+		defer file.Close()
+		if err2!=nil{ log.Fatal(err) }
+		err3 := ioutil.WriteFile("config.json",
+								 []byte("{\n\t"+
+								 		`"username":"ndraey",`+"\n\t"+
+								 		`"currency":"none",`+"\n\t"+
+								 		`"threads":"auto",`+"\n\t"+
+								 		`"feed-every":45`+"\n"+
+								 		"}\n"),
+								 0644)
+		if err3!=nil { log.Fatal(err) }
+	}
+
+	rawconfig, err := ioutil.ReadFile(defconfigfile)
+	if err!=nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	json.Unmarshal(rawconfig,&config)
+
+	if config.Username!="" {username = config.Username}
+	if config.Currency!="" {additconv = config.Currency}
+	if config.Threads!="" {
+		if config.Threads!="auto" {
+			threads = int(config.ThreadsInt)
+		}
+	}
+	if config.FeedEvery!=0 { feedevery = time.Duration(config.FeedEvery) }
+	
 	bannerw:=42
 	unlen:=14+len(username)
 	threadslen:=12+len(strconv.Itoa(threads))
+	currlen:=21+len(additconv)
 	print("------------------------------------------\n")
 	print("| PulseMiner - DUCO Miner written in Go  |\n")
 	print("|                                        |\n")
 	print("| Mining to: "+username+strings.Repeat(" ",bannerw-unlen)+"|\n")
 	print("| Threads: "+strconv.Itoa(threads)+strings.Repeat(" ",bannerw-threadslen)+"|\n")
+	print("| Cuurency to show: "+additconv+strings.Repeat(" ",bannerw-currlen)+"|\n")
+	print("| Show balance every: "+strconv.Itoa(int(feedevery))+" seconds"+strings.Repeat(" ",bannerw-currlen-9)+"|\n")
 	print("------------------------------------------\n")
+
+
+	
 	print("\nGetting best pool...\n")
-	tothashrate := make([]int,threads)
+	tothashrate := make([]float64,threads)
 	pooldata, err := getnet("https://server.duinocoin.com/getPool")
 	if err!=nil {print("Error getting pool! Press Ctrl-C to exit...\n"); for{}}
 	json.Unmarshal(pooldata,&pool)
 	fmt.Printf("Selected pool %s:%d\n",pool.Ip,pool.Port)
 	print("Connecting...\n")
 	rand.Seed(time.Now().UnixNano())
-	minerid:=rand.Int63()%1712
+	minerid:=rand.Int31()%1712
+
+
+	
 	for i:=0; i<threads; i++{
 		go mine(pool.Ip,pool.Port,minerid,i,tothashrate)
 	}
@@ -129,11 +223,18 @@ func main(){
 		pricedata, err := getnet("https://server.duinocoin.com/statistics")
 		if err!=nil {log.Fatal(err)}
 		json.Unmarshal(pricedata, &priced)
-		
-		fmt.Printf("[feed] Balance: %.5f DUCO (≈%.5f USD)\n",
-					balance,
-					balance*priced.DUCO)
-		time.Sleep(30*time.Second)
+
+		if additconv!="none" {
+			fmt.Printf("[feed] Balance: %.5f DUCO (≈%.5f USD)"+
+						currconv(balance*priced.DUCO)+"\n",
+						balance,
+						balance*priced.DUCO)
+		}else{
+			fmt.Printf("[feed] Balance: %.5f DUCO (≈%.5f USD)\n",
+						balance,
+						balance*priced.DUCO)
+		}
+		time.Sleep(feedevery*time.Second)
 	}
 }
 
@@ -143,15 +244,30 @@ func getnet(url string) ([]byte, error) {
 	return body, err
 } 
 
-func sum(array []int) int {  
-	result := 0  
-	for _, v := range array {  
-		result += v  
+func sum(array []float64) float64 {
+	result := float64(0)
+	for _, v := range array {
+		result += v
 	}  
 	return result  
 }
 
-func mine(ip string, port int32, minerid int64, cpuid int, thr []int){
+func currconv(value float32) string {
+	st1, err := getnet("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	json.Unmarshal(st1,&currency)
+	r := reflect.ValueOf(currency.Usd)
+	res := reflect.Indirect(r).FieldByName(strings.Title(additconv))
+
+	totalresp:=fmt.Sprintf(" (%.5f %s)",
+						   float32(res.Float())*value,
+						   strings.ToUpper(additconv))
+	return totalresp
+}
+
+func mine(ip string, port, minerid int32, cpuid int, thr []float64){
 	print("Started...\n")
 	conn, _ := net.Dial("tcp",ip+":"+strconv.Itoa(int(port)))
 	ver := make([]byte,10)
@@ -205,16 +321,16 @@ func RemoveIndex(s []string, i int) []string {
     return s[:i+copy(s[i:], s[i+1:])]
 }
 
-func convr(num int) string {
+func convr(num float64) string {
 	rts:=""
 	if num>(1000*1000*1000){
-		k := strconv.Itoa(int(num/(1000*1000*1000)))
+		k := strconv.FormatFloat(num/(1000*1000*1000),'f',2,64)
 		rts = k+"G"
 	}else if num>(1000*1000) {
-		k := strconv.Itoa(int(num/(1000*1000)))
+		k := strconv.FormatFloat(num/(1000*1000),'f',2,64)
 		rts = k+"M"
 	}else if(num>1000){
-		k := strconv.Itoa(int(num/1000))
+		k := strconv.FormatFloat(num/1000,'f',2,64)
 		rts = k+"k"
 	}else{
 		rts = strconv.Itoa(int(num))
@@ -222,9 +338,9 @@ func convr(num int) string {
 	return rts
 }
 
-func DecodeHash(prev string,result string,diff int) (int, int, float64) {
+func DecodeHash(prev string,result string,diff int) (int, float64, float64) {
 	i:=int(0)
-	hr := int(0)
+	hr := float64(0)
 	tm := float64(gettime()/1000)
 	tmm := float64(0)
 	for i=0; i<(100*diff+1); i++ {
@@ -233,7 +349,7 @@ func DecodeHash(prev string,result string,diff int) (int, int, float64) {
 		if hashd==result {
 			tmm = float64(gettime()/1000)-tm
 			if tmm<=1 { tmm = 1 }
-			hr = i/int(tmm)
+			hr = float64(i)/tmm
 			break
 		}
 	}
